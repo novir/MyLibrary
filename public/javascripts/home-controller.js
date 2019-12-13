8ce657032b9e0db593b7c2757bbeba6d63f9c05a
novir <p.pluta.86@gmail.com>
2019-12-13 15:21:39 +0100

    Changed users view

--- public/javascripts/home-controller.js
+++ public/javascripts/home-controller.js
@@ -1,39 +1,32 @@
 angular.module('myLib')
     .controller('HomeController', function HomeController($http, $window) {
         const vm = this;
-
         vm.listUsers = function () {
             $http.get('http://localhost:9000/users').then(function (response) {
                 vm.users = response.data;
             });
         };
-
         vm.addUser = function (user) {
             let userData = JSON.stringify(user);
             $http.post('http://localhost:9000/users', userData);
             vm.listUsers();
         };
-
         vm.updateUser = function (user) {
             let id = user.id;
             let userData = JSON.stringify(user);
             $http.post('http://localhost:9000/users/' + id, userData);
             vm.listUsers();
         };
-
         vm.removeUser = function (user) {
             let id = user.id;
             $http.delete('http://localhost:9000/users/' + id);
             vm.listUsers();
         };
-
         vm.showExtraRow = function () {
             vm.extraRowPresent = true;
         };
-
         vm.hideExtraRow = function () {
             vm.extraRowPresent = false;
         };
-
         vm.listUsers();
     });
